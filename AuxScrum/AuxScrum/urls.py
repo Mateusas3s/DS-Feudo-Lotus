d73d3049b28d3d279dd0490060e0da1917bfc2cb
Guilherme D. Sá <guilhermedsa.spike13@gmail.com>
2022-03-15 17:23:08 -0300

    inicio cadastro, Co-authored-by: GuilhermeDSa1013 <guilhermedsa.spike13@gmail.com> Co-authored-by: gaubiela <gabrielaalves.gsa@gmail.com>

--- AuxScrum/AuxScrum/urls.py
+++ AuxScrum/AuxScrum/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cadastro import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', v.cadastro, name='cadastro'),
 ]
