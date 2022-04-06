6c9d1609d7291451dc4e6fa6d2348778e25f278c
Guilherme D. Sá <guilhermedsa.spike13@gmail.com>
2022-04-06 08:28:38 -0300

    início da pag de Projeto

--- AuxScrum/cadastros/urls.py
+++ AuxScrum/cadastros/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import PaginaProjeto, ProjetoCreate, ProjetoDelete, ProjetoList, ProjetoUpdate
+from .views import ProjetoList, ProjetoDelete
+#  ProjetoCreate,  , ProjetoUpdate
 from . import views
 
 
 urlpatterns = [
     #path('', view, name=''),
     path('home/', ProjetoList.as_view(), name='home'),
-    path('cadastrar/projeto/', ProjetoCreate.as_view(), name='cadastrar-projeto'),
-    path('editar/projeto/<int:pk>', ProjetoUpdate.as_view(), name='editar-projeto'),
+    # path('cadastrar/projeto/', ProjetoCreate.as_view(), name='cadastrar-projeto'),
+    # path('editar/projeto/<int:pk>', ProjetoUpdate.as_view(), name='editar-projeto'),
     path('excluir/projeto/<int:pk>', ProjetoDelete.as_view(), name='excluir-projeto'),
-    path('projeto/<int:pk>', PaginaProjeto.as_view(), name='pagina-projeto'),
+    path('projeto/<int:pk>', views.paginaProjeto, name='pagina-projeto'),
+    path('cadastrar/projeto/', views.cadastrarProjeto, name='cadastrarProjeto'),
+    path('editar/projeto/<int:pk>', views.editarProjeto, name='editarProjeto'),
 ]
--- AuxScrum/cadastros/views.py
+++ AuxScrum/cadastros/views.py
@@ -1,4 +1,5 @@
 from audioop import reverse
+from multiprocessing import context
 from re import template
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -8,35 +9,35 @@ from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 
 from .models import Projeto
+from .forms import ProjetoForm
+
+# #Create your views here.
+
+# # Create
+
+# class ProjetoCreate(CreateView):
+#     model = Projeto
+#     fields = ['nome', 'descricao', 'membros']
+#     template_name = 'cadastros/cadastrarProjeto.html'
+#     success_url = reverse_lazy('home')
 
 
-#Create your views here.
+# # Update
 
-# Create
+# class ProjetoUpdate(UpdateView):
+#     model = Projeto
+#     fields = ['nome', 'descricao', 'membros']
+#     template_name = 'cadastros/editarProjeto.html'
+#     success_url = reverse_lazy('home')
 
-class ProjetoCreate(CreateView):
-    model = Projeto
-    fields = ['nome', 'descricao', 'membros']
-    template_name = 'cadastros/cadastrarProjeto.html'
-    success_url = reverse_lazy('home')
-
-
-# Update
-
-class ProjetoUpdate(UpdateView):
-    model = Projeto
-    fields = ['nome', 'descricao', 'membros']
-    template_name = 'cadastros/editarProjeto.html'
-    success_url = reverse_lazy('home')
-
-# Delete
+# # Delete
 
 class ProjetoDelete(DeleteView):
     model = Projeto
     template_name = 'cadastros/excluirProjeto.html'
     success_url = reverse_lazy('home')
 
-# List
+# # List
 
 class ProjetoList(ListView):
     model = Projeto
@@ -46,14 +47,41 @@ class ProjetoList(ListView):
         self.object_list = Projeto.objects.filter(membros=self.request.user)
         return self.object_list
 
-class PaginaProjeto(CreateView):
-    model = Projeto
-    fields = ['nome', 'descricao', 'membros']
-    template_name = 'cadastros/paginaProjeto.html'
+def paginaProjeto(request, pk):
+    projeto = Projeto.objects.get(id=pk)
+    context = {
+        'projeto': projeto
+    }
+    return render(request, 'cadastros/paginaProjeto.html', context)
 
-    def get_context_data(self, *args, **kwargs):
-        return super().get_context_data(*args, **kwargs)
 
+def cadastrarProjeto(request):
+    form = ProjetoForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    context = {
+        'form': form
+    }
+    return render(request, 'cadastros/cadastrarProjeto.html', context)
+
+def editarProjeto(request, pk):
+    projeto = Projeto.objects.get(id=pk)
+    form = ProjetoForm(instance=projeto)
+    if request.method == 'POST':
+        form = ProjetoForm(request.POST, instance=projeto)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {
+        'form': form,
+        'projeto': projeto,
+    }
+    return render(request, 'cadastros/editarProjeto.html', context)
+
+
+
+    
         
     
 
