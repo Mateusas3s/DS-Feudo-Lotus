69bfb01eab0ed5cc49ccd59438bf05b6f4450770
Guilherme D. Sá <guilhermedsa.spike13@gmail.com>
2022-04-14 12:40:07 -0300

    Template das páginas da funcionalidade das tarefas, Co-authored-by: GuilhermeDSa1013 <guilhermedsa.spike13@gmail.com> Co-authored-by: gaubiela <gabrielaalves.gsa@gmail.com> Co-authored-by: OscarDeBrito <oscarcorreiadebrito@gmail.com> Co-authored-by: storch7 <guilhermestorch7@gmail.com>

--- AuxScrum/tarefas/views.py
+++ AuxScrum/tarefas/views.py
@@ -4,27 +4,30 @@ from .forms import AdicionarTarefa, EditarTarefaForm
 
 def tarefas_pendentes_list(request):
     tarefas_pendentes = Tarefa.objects.filter(status='pendente')
-    if request.method == 'POST':
-        form = AdicionarTarefa(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('tarefas_pendentes_list')
-    else:
-        form = AdicionarTarefa()
+    
     return render(request, 'tarefas/tarefas_pendentes.html',
-                  {'tarefas_pendentes':tarefas_pendentes,
-                   'form':form})
+                  {'tarefas_pendentes':tarefas_pendentes,})
                    
 def concluir_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
     tarefa.status = 'concluído'
     tarefa.save()
-    return redirect('tarefas_pendentes_list')
+    return redirect('tarefas_progresso_list')
 
-def excluir_tarefa(request, tarefa_id):
+def excluir_tarefa_pendente(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
     tarefa.delete()
     return redirect('tarefas_pendentes_list')
+
+def excluir_tarefa_progresso(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.delete()
+    return redirect('tarefas_progresso_list')
+
+def excluir_tarefa_concluidas(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.delete()
+    return redirect('tarefas_concluidas_list')
 
 def adiar_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
@@ -32,7 +35,7 @@ def adiar_tarefa(request, tarefa_id):
     tarefa.save()
     return redirect('tarefas_pendentes_list')
 
-def editar_tarefa(request, tarefa_id):
+def editar_tarefa_pendente(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
     if request.method == 'POST':
         form = EditarTarefaForm(request.POST)
@@ -44,7 +47,21 @@ def editar_tarefa(request, tarefa_id):
             return redirect('tarefas_pendentes_list')
     else:
         form = EditarTarefaForm(initial={'tarefa':tarefa.descricao, 'categoria':tarefa.categoria})
-    return render(request, 'tarefas/editar_tarefa.html', {'tarefa':tarefa, 'form':form})
+    return render(request, 'tarefas/editar_tarefa_pendente.html', {'tarefa':tarefa, 'form':form})
+
+def editar_tarefa_progresso(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    if request.method == 'POST':
+        form = EditarTarefaForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            tarefa.descricao = cd['tarefa']
+            tarefa.categoria = cd['categoria']
+            tarefa.save()
+            return redirect('tarefas_progresso_list')
+    else:
+        form = EditarTarefaForm(initial={'tarefa':tarefa.descricao, 'categoria':tarefa.categoria})
+    return render(request, 'tarefas/editar_tarefa_progresso.html', {'tarefa':tarefa, 'form':form})
 
 def tarefas_concluidas_list(request):
     tarefas_concluidas = Tarefa.objects.filter(status='concluído')
@@ -54,8 +71,24 @@ def tarefas_adiadas_list(request):
     tarefas_adiadas = Tarefa.objects.filter(status='adiado')
     return render(request, 'tarefas/tarefas_adiadas.html', {'tarefas_adiadas':tarefas_adiadas})
 
-def mover_para_tarefas(request, tarefa_id):
+def mover_para_pendente(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
     tarefa.status = 'pendente'
     tarefa.save()
-    return redirect('tarefas_pendentes_list')
+    return redirect('tarefas_progresso_list')
+
+def mover_para_progresso(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.status = 'adiado'
+    tarefa.save()
+    return redirect('tarefas_concluidas_list')
+
+def tarefaPage(request):
+    if request.method == 'POST':
+        form = AdicionarTarefa(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tarefas_page')
+    else:
+        form = AdicionarTarefa()
+    return render(request, 'tarefas/tarefas.html', context={'form':form})
